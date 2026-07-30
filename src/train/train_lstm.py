f39c91657c9e1fc1b97147320a31d3bb16c95b6c
Jasmine Erell <jasmine.erell@post.runi.ac.il>
2026-07-30 14:28:24 +0300

    Add evaluation folder and fix train LSTM to return weights

--- src/train/train_lstm.py
+++ src/train/train_lstm.py
@@ -1,9 +1,10 @@
 import json
+import torch
 from pathlib import Path
 from src.models.lstm import MusicLSTMModel
-from src.evaluator import MusicEvaluator
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+
 
 def load_json(relative_path):
     full_path = PROJECT_ROOT / relative_path
@@ -15,26 +16,26 @@ def load_json(relative_path):
                 data.append(json.loads(line))
     return data
 
-def main():
-    print("1. Loading datasets...")
-    train_data = load_json("data/splits/train.jsonl")
-    masked_val_data = load_json("data/splits/validation_masked_factorized.jsonl")
 
-    # Extract just the sequences for training
-    print("Available keys in dataset:", train_data[0].keys())
+def main():
+    print("1. Loading training dataset...")
+    train_data = load_json("data/splits/train.jsonl")
+
     train_sequences = [item["factorized_sequence_tokens"] for item in train_data]
     vocab = list(set([token for seq in train_sequences for token in seq]))
 
-    print("\n2. Initializing Models...")
-    models = {
-        "LSTM": MusicLSTMModel(vocab=vocab, embed_dim=64, hidden_dim=128)
-    }
+    print("\n2. Initializing LSTM Model...")
+    lstm_model = MusicLSTMModel(vocab=vocab, embed_dim=64, hidden_dim=128)
 
-    print("\n3. Training Models...")
-    for name, model in models.items():
-        print(f"Training {name}...")
-        model.fit(train_sequences, epochs=5, batch_size=32)
+    print("\n3. Training Model...")
+    lstm_model.fit(train_sequences, epochs=5, batch_size=32)
 
+    print("\n4. Saving trained LSTM weights...")
+    weight_dir = PROJECT_ROOT / "results"
+    weight_dir.mkdir(parents=True, exist_ok=True)
+
+    torch.save(lstm_model.model.state_dict(), weight_dir / "lstm_checkpoint.pt")
+    print(f"Checkpoint saved successfully to {weight_dir / 'lstm_checkpoint.pt'}!")
 
 
 if __name__ == "__main__":
